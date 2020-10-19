73771561154b008bc00f0787ef30384a1a3e3fb1
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-10-19 19:04:51 +0530

    jtjxoncommit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,37 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    const givenData = {
+      type: "div",
+      name: "Clock",
+      root: true,
+      style: {
+        display: "flex",
+        "flex-direction": "row",
+        "justify-content": "center"
+      },
+      children: [
+        {
+          type: "div",
+          name: "Hour",
+          style: {},
+          children: []
+        },
+        {
+          type: "span",
+          name: "Minute",
+          style: {
+            color: "green",
+            "font-size": 30
+          },
+          children: []
+        }
+      ]
+    };
+    return <></>;
+  }
 }
-
 
 export default App;
